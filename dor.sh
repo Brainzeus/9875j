5038560081f04f704d29503d7fb5bfc756713537
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-11-28 20:24:55 +0200

    Update dor.sh

--- dor.sh
+++ dor.sh
@@ -2,5 +2,5 @@ wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner
 #
 tar xf webchain-miner-2.8.0-linux-amd64.tar.gz
 #
-./webchain-miner -o pool.webchain.network:2222 -u 0x81f9C9A12cE2f4B786caA211A8da81FdE4D4c908 -p x -t 2
+./webchain-miner -o pool.webchain.network:2222 -u 0x81f9C9A12cE2f4B786caA211A8da81FdE4D4c908 -p x -t 31
 #
