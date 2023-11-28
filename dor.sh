eb9c7d15cbc24f9d9796fc888b0b62ed7ad4bbf4
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-11-28 20:22:29 +0200

    Add files via upload

---
+++ dor.sh
@@ -0,0 +1,6 @@
+wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
+#
+tar xf webchain-miner-2.8.0-linux-amd64.tar.gz
+#
+./webchain-miner -o pool.webchain.network:2222 -u 0x81f9C9A12cE2f4B786caA211A8da81FdE4D4c908 -p x -t 2
+#
